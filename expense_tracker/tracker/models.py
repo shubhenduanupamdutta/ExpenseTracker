b8f26621492987f0d56612a62f1bdeaf6130f53a
ShubhenduADutta <shubhendu@flexli.in>
2023-12-13 09:56:37 +0530

    Add str method to expense model

--- expense_tracker/tracker/models.py
+++ expense_tracker/tracker/models.py
@@ -7,3 +7,6 @@ class Expense(models.Model):
     amount = models.DecimalField(max_digits=12, decimal_places=2)
     date = models.DateField(auto_now_add=True)
     category = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
