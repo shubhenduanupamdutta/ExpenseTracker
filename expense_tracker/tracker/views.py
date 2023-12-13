4ca4834263063e18fa2101ec72ce6851e10c1b16
ShubhenduADutta <shubhendu@flexli.in>
2023-12-13 16:12:55 +0530

    handle post request from expense form

--- expense_tracker/tracker/views.py
+++ expense_tracker/tracker/views.py
@@ -1,6 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from .forms import ExpenseForm
 
 
 # Create your views here.
 def index(request):
-    return render(request, "tracker/index.html")
+    if request.method == "POST":
+        form = ExpenseForm(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect("index")
+    form = ExpenseForm()
+    return render(request, "tracker/index.html", {"form": form})
